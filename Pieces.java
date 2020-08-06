16539cf4ec9ca1a0307f67c50b8d89ce5bd8490a
jkxfam11 <67762703+jkxfam11@users.noreply.github.com>
2020-08-06 14:11:23 -0700

    Update Pieces.java

--- Pieces.java
+++ Pieces.java
@@ -1,1 +1,9 @@
-
+public class Pieces {
+  private final int red = 0;
+  private final int black = 1;
+  private final int redKing = 2;
+  private final int blackKing = 3;
+  public static void main (String[] args){
+   
+  }
+}
