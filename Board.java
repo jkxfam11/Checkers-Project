9258266d4373d567855cb631ebfbfd6bf41d4f38
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-08-06 13:35:39 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,6 +1,14 @@
-public class Board
+/*
+ * Authors: Harold Pham and Kevin Xu
+ * Creation Date: 8/6/2020
+ * File Details: Will be the Board class that constructs every new game and holds game data.
+*/
+public class Board 
 {
-
-
-
+	
+	public Board()
+	{
+		
+	}
+  
 }
