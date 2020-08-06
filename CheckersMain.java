114e64fe41d0858b83de02f7d6faf36d9a459e8e
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-08-06 13:57:09 -0700

    Transfer to Board.java

--- CheckersMain.java
+++ CheckersMain.java
@@ -9,26 +9,9 @@ import javax.swing.*;
 
 public class CheckersMain extends JFrame 
 {
-	public int currentPlayer = 0; //0 == WHITE/P1   1 = BLACK/P2
-	//private static int final 
-	//int white = 0; int black =1;
-	public int currentTurn; //Counts the turns, can be displayed at the upper right
-	
+
 	public static void main(String[] args) 
 	{
-		//Create the Objects
-		DrawingPanel p = new DrawingPanel(700,500);
-		Graphics g = p.getGraphics();
-		Image p1B = p.loadImage(new File("Board.jpg")); 
-		
-		//Set the Colors
-		p.setBackground(Color.BLUE);
-		g.setColor(Color.BLACK);
-		
-		
-		g.drawImage(p1B,100,0,p);
-		
-		
-	}
+
 
 }
