ec681869dd77c521b1bf2cc809ef7e0faf6dc79f
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-08-07 13:48:36 -0700

    Turn Display Update
    The display looks nicer and is very centered now.

--- Board.java
+++ Board.java
@@ -7,6 +7,7 @@ import javax.swing.*;
  * Creation Date: 8/6/2020
  * File Details: Will be the Board class that constructs every new game and holds game data.
 */
+
 public class Board 
 {
 //VARIABLES
@@ -27,12 +28,14 @@ public class Board
 		Image p1B = p.loadImage(new File("CheckerBoard.jpg")); 
 				
 		//Set the Colors
-		p.setBackground(Color.BLUE);
+		p.setBackground(Color.gray);
 		g.setColor(Color.BLACK);
 				
 		g.drawImage(p1B,100,0,p); //Makes the checker Board
 		
+		g.drawRect(1, 18, 96, 35);
 		
+		displayPlayer(1,15);
 	}
 	
 //METHODS
@@ -51,10 +54,10 @@ public class Board
 //Mutator Methods
 	public void nextPlayer()
 	{
-		if(currentPlayer == 0)
+		if(currentPlayer == 0) //set to RED Player
 			currentPlayer = 1;
 		else
-			currentPlayer = 0;
+			currentPlayer = 0; //Set to BLACK Player
 	}
 	
 	public void nextTurn()
@@ -66,10 +69,21 @@ public class Board
 	//Converts our binary player variable into a String at a given spot on the window.
 	public void displayPlayer(int x, int y)
 	{
+		g.setFont(new Font("Monospaced", Font.BOLD,18));//Set text size/font
+		
+		g.drawString("Your turn", x, y); //Note: It was easier to just use two separate msgs.
+		
 		if(currentPlayer == 0)
-			g.drawString("P1",x,y);
+			g.drawString("BLACK",x+23,y+26);
 		else
-			g.drawString("P2",x,y);
+		{
+			g.setColor(Color.RED);
+			g.drawString("RED",x+33,y+27);
+			g.setColor(Color.BLACK);
+		}
 	}
+		
+	
+	
 	
 }
