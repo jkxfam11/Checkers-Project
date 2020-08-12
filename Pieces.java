08f6ecb79e16bbabf3de3fcf10e59e51edfd578b
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-08-12 13:27:00 -0700

    Update Pieces.java

--- Pieces.java
+++ Pieces.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Graphics;
 /*
  * Authors: Kevin Xu and Harold Pham
  * Creation Date: 8/11/2020
@@ -10,52 +12,68 @@ public class Pieces
 {
 	private final int EMPTY = 0;
 	private final int RED = 1;
-	private final int BLACK = 2;
+	private final int WHITE = 2;
 	private final int RED_KING = 3;
 	private final int BLACK_KING = 4;
 	
 	private int[][] board = new int[8][8]; //Defaults to empty spaces
 	
-	public Pieces()
+	public Pieces(Graphics g, DrawingPanel p)
 	{
+		int w = 107;
+		int h = 8;
     //populate red's pieces
-		for(int r = 0; r < 2; r++)
+		g.setColor(Color.red);
+		for(int c = 0; c < 3; c++)
 		{
-				if(r % 2 == 1) //if it is an odd row
+				if(c % 2 == 1) //if it is an odd column
 				{
-					for(int c = 0; c < 8; c++)
+					for(int r = 0; r < 8; r++)
 					{
-						if(c % 2 == 0)//then every even column space will have a piece
+						if(r % 2 == 0)//then every even row space will have a piece
+						{
 							board[r][c] = 1;
+							g.fillOval(62*r+w, 62*c+h, 50, 50);
+						}
 					}
 				}
-				else if(r % 2 == 0) //if it is an even row
+				else if(c % 2 == 0) //if it is an even column
 				{
-					for(int c = 0; c < 8; c++)
+					for(int r = 0; r < 8; r++)
 					{
-						if(c % 2 == 1) //then every odd column space will have a piece
+						if(r % 2 == 1) //then every odd row space will have a piece
+						{
 							board[r][c] = 1;
+							g.fillOval(62*r+w, 62*c+h, 50, 50);
+						}
 					}
 				}	
 		}
 		
-    //populate black's pieces
-		for(int r = 5; r < 8; r++) 
+    //populate white's pieces
+		g.setColor(Color.white);
+		for(int c = 5; c < 8; c++) 
 		{
-				if(r % 2 == 1) //if it is an odd row
+				if(c % 2 == 1) //if it is an odd column
 				{
-					for(int c = 0; c < 8; c++)
+					for(int r = 0; r < 8; r++)
 					{
-						if(c % 2 == 0)//then every even column space will have a piece
+						if(r % 2 == 0)//then every even row space will have a piece
+						{
 							board[r][c] = 2;
+							g.fillOval(62*r+w, 62*c+h, 50, 50);
+						}
 					}
 				}
-				else if(r % 2 == 0) //if it is an even row
+				else if(c % 2 == 0) //if it is an even column
 				{
-					for(int c = 0; c < 8; c++)
+					for(int r = 0; r < 8; r++)
 					{
-						if(c % 2 == 1) //then every odd column space will have a piece
+						if(r % 2 == 1) //then every odd row space will have a piece
+						{
 							board[r][c] = 2;
+							g.fillOval(62*r+w, 62*c+h, 50, 50);
+						}
 					}
 				}	
 		}
