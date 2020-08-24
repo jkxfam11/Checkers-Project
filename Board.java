42bd1a7aca4484552a168a5a5077c276ec2e71ae
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-08-24 14:34:12 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -86,6 +86,16 @@ public class Board extends JPanel
 		return currentPlayer;
 	}
 	
+	public Graphics getG()
+	{
+		return g;
+	}
+	
+	public DrawingPanel getP()
+	{
+		return p;
+	}
+	
 //Mutator Methods
 	public void nextPlayer()
 	{
