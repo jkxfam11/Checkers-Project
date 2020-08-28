6a5f9f853266938408aa15ba901a91326295f907
jkxfam11 <67762703+jkxfam11@users.noreply.github.com>
2020-08-28 13:19:20 -0700

    Update CheckersMain.java
    Added a link for research

--- CheckersMain.java
+++ CheckersMain.java
@@ -2,6 +2,7 @@
  * Authors: Kevin Xu and Harold Pham
  * File Creation Date: 8/3/2020
  * File Detail: The main class which will run instances from the other classes to reinact a Checkers game.
+ * https://www.cis.upenn.edu/~matuszek/cit594-2012/Pages/BoardGame.html
 */
 
 import java.awt.*;
